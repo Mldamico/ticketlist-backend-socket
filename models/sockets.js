6a72dc9f2fc6efd86982a05a13b8f099b19ad6d3
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-12-29 08:32:26 -0300

    Cola de tickets

--- models/sockets.js
+++ models/sockets.js
@@ -1,4 +1,4 @@
-const TicketList = require("./ticketList");
+const TicketList = require('./ticketList');
 class Sockets {
   constructor(io) {
     this.io = io;
@@ -8,17 +8,18 @@ class Sockets {
 
   socketEvents() {
     // On connection
-    this.io.on("connection", socket => {
-      socket.on("solicitar-ticket", (data, callback) => {
+    this.io.on('connection', (socket) => {
+      socket.on('solicitar-ticket', (data, callback) => {
         const nuevoTicket = this.ticketList.crearTicket();
         callback(nuevoTicket);
       });
-      socket.on("siguiente-ticket-trabajar", (usuario, callback) => {
+      socket.on('siguiente-ticket-trabajar', (usuario, callback) => {
         const suTicket = this.ticketList.asignarTicket(
           usuario.agente,
           usuario.escritorio
         );
         callback(suTicket);
+        this.io.emit('ticket-asignado', this.ticketList.ultimos13);
       });
     });
   }
